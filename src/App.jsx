20d540b597e34d53c3328c8b7ca0568772e6379f
Gustavo Morini <gustavommh@gmail.com>
2022-10-08 13:19:00 -0300

    ajust app components and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 import React, { useEffect, useState } from "react";
-
-import { homeList } from "./api/Tmdb";
+import "./App.css";
+import MovieRow from "./components/MovieRow/index";
+import MovieFeature from "./components/MovieFeature";
+import { homeList, getFeature } from "./api/Tmdb";
+import { randomArray } from "./helpers/index";
 
 export default () => {
   const [list, setList] = useState([]);
+  const [feature, setFeature] = useState(null);
 
   useEffect(() => {
     const getAll = async () => {
+      // list
       const list = await homeList();
+      // console.log("LIST ---- ", list);
       setList(list);
+      // feature
+      const filter = list.filter(
+        (item) => item.slug == "series" || item.slug == "movies"
+      );
+      const randomFeature = randomArray(filter);
+      const randomFeatureItem = randomArray(randomFeature.items.results);
+      const data = await getFeature(randomFeatureItem.id, randomFeature.slug);
+      setFeature(data);
     };
     getAll();
   }, []);
 
-  return <div className="page"></div>;
+  return (
+    <div className="page">
+      {feature && <MovieFeature item={feature} />}
+      <section className="list">
+        {list.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
