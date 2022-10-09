b39bd067f418edffcc469296272ea3fc865336d3
Gustavo Morini <gustavommh@gmail.com>
2022-10-09 09:02:42 -0300

    loader

---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div>
+        <p></p>
+        <span>Carregando...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
