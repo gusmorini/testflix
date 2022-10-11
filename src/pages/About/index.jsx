77c62437f94d25fac882ccfd40a2711c0c2d0a1d
Gustavo Morini <gustavommh@gmail.com>
2022-10-11 08:40:51 -0300

    about - ajust similar, recommends

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,30 +2,40 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./style.css";
 
-import { getFeature } from "../../api/Tmdb";
+import { getFeature, getSimilar, getRecommendations } from "../../api/Tmdb";
 
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
+import MovieTitle from "../../components/MovieTitle";
+import MovieInfo from "../../components/MovieInfo";
+import MovieGenres from "../../components/MovieGenres";
+import MovieList from "../../components/MovieList";
+
+import Loader from "../../components/Loader";
+
 const About = () => {
   const { id, title } = useParams();
   const [item, setItem] = useState({});
+  const [itemSimilar, setItemSimilar] = useState([]);
+  const [listRecommends, setListRecommends] = useState([]);
   const [background, setBackground] = useState("");
   const [poster, setPoster] = useState("");
-  const [genres, setGenres] = useState([]);
 
   const getList = async (id, title) => {
-    const data = await getFeature(id, title);
-    console.log("DATA -- ", data);
-    setItem(data);
-    setBackground("https://image.tmdb.org/t/p/original" + data.backdrop_path);
-    setPoster("https://image.tmdb.org/t/p/w300" + data.poster_path);
+    const { feature } = await getFeature(id, title);
 
-    let gen = [];
-    for (let x in data.genres) {
-      gen.push(data.genres[x].name);
-    }
-    setGenres(gen);
+    const similar = await getSimilar(feature.id, feature.slug);
+    const recommendations = await getRecommendations(feature.id, feature.slug);
+
+    setItem(feature);
+    setItemSimilar(similar.results);
+    setListRecommends(recommendations.results);
+
+    setBackground(
+      "https://image.tmdb.org/t/p/original" + feature.backdrop_path
+    );
+    setPoster("https://image.tmdb.org/t/p/w300" + feature.poster_path);
   };
 
   useEffect(() => {
@@ -36,32 +46,38 @@ const About = () => {
 
   return (
     <div>
+      {!itemSimilar.length > 0 && !background && <Loader />}
       <Header />
       <div
         className="about--hero"
         style={{ backgroundImage: `url(${background})` }}
       ></div>
-      {/* <MovieFeature item={list} /> */}
       <div
         className="about container"
         style={{
-          transform: `translateY(-${window.innerHeight / 2}px)`,
+          transform: `translateY(-${window.innerHeight / 3}px)`,
         }}
       >
-        <img
-          src={poster}
-          alt={item.original_name || item.original_title}
-          className="poster"
-        />
-        <div className="content">
-          <h1 className="title">{item.title || item.name}</h1>
-          <h4 className="original">
-            {item.original_title || item.original_name}
-          </h4>
-          <p className="overview">{item.overview || "..."}</p>
-          <h4 className="genres">Genero: {genres.join(", ")}</h4>
+        <div className="box">
+          <img
+            src={poster}
+            alt={item.original_name || item.original_title}
+            className="poster"
+          />
+          <div className="content">
+            <MovieTitle item={item} />
+            <MovieInfo item={item} />
+            <p className="overview">{item.overview || "..."}</p>
+            <MovieGenres item={item} />
+          </div>
         </div>
+
+        <hr />
+
+        <MovieList items={listRecommends} title="recomendados" />
+        <MovieList items={itemSimilar} title="similares" />
       </div>
+
       <Footer />
     </div>
   );
