57b9a1dfa954d9c8852f73312eb92c15e301769a
Gustavo Morini <gustavommh@gmail.com>
2022-10-11 08:39:59 -0300

    movieList

---
+++ src/components/MovieList/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const MovieList = ({ items, title }) => {
+  const base_img = "https://image.tmdb.org/t/p/w300";
+
+  const renderItem = (item, key) => {
+    return (
+      <div key={key} className="item">
+        <a
+          href={`/about/${item.id}/${
+            item.original_title || item.original_name
+          }`}
+        >
+          <img
+            src={base_img + item.poster_path}
+            alt={item.title || item.name}
+          />
+        </a>
+      </div>
+    );
+  };
+
+  return (
+    <div className="similar">
+      <h3 className="title">{title}</h3>
+      <div className="list">
+        {items.length > 0
+          ? items.map((item, key) => renderItem(item, key))
+          : "nada encontrado"}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
