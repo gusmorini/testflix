754dffc0af040812537cbe5c68ea6fa87164d29a
Gustavo Morini <gustavommh@gmail.com>
2022-10-11 05:38:41 -0300

    components

--- src/components/MovieFeature/index.jsx
+++ src/components/MovieFeature/index.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import "./style.css";
 import { limitText } from "../../helpers/index";
 
+import MovieTitle from "../MovieTitle";
+import MovieInfo from "../MovieInfo";
+import MovieOverview from "../MovieOverview";
+import Genres from "../MovieGenres";
+
 const MovieFeature = ({ item }) => {
   const url_image = "https://image.tmdb.org/t/p/original" + item.backdrop_path;
-  const vote = ((item.vote_average * 1000) / 100).toFixed(0);
-  const initial_date = item.first_air_date || item.release_date;
-  const date = initial_date ? initial_date.split("-")[0] : "";
-  const genres = [];
-  for (let n in item.genres) {
-    genres.push(item.genres[n].name);
-  }
-
-  const cont = window.innerHeight <= 360 ? 100 : 360;
 
   return (
     <div className="feature">
@@ -23,33 +19,10 @@ const MovieFeature = ({ item }) => {
 
       <div className="container">
         <div className="feature--box">
-          <h1 className="feature--title">{item.name || item.title}</h1>
-          <div className="feature--info">
-            <p className="info--relevance">{vote}% Relevância</p>
-            <p className="info--year">{date}</p>
-            <p className="info--duration">
-              {item.number_of_seasons
-                ? `${item.number_of_seasons} ${
-                    item.number_of_seasons > 1 ? "temporadas" : "temporada"
-                  }`
-                : `${item.runtime} min`}
-            </p>
-          </div>
-          <p className="feature--overview">
-            {limitText(item.overview || "...", cont)}
-          </p>
-          <div className="feature--buttons">
-            <a className="play" href="#">
-              assistir
-            </a>
-            <a className="mylist" href="#">
-              minha lista
-            </a>
-          </div>
-          <div className="feature--genres">
-            <strong>Gêneros: </strong>
-            <span>{genres.join(", ")}</span>
-          </div>
+          <MovieTitle item={item} />
+          <MovieInfo item={item} />
+          <MovieOverview item={item} />
+          <Genres item={item} />
         </div>
       </div>
     </div>
